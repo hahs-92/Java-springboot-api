4f410d6f769634d1e3676127891242dc2eebe57b
Alex <alexandre_2420@outlook.com>
2021-12-31 15:10:19 -0500

    add Hi World example

---
+++ src/main/java/com/platzi/market/controller/HiWorld.java
@@ -0,0 +1,14 @@
+package com.platzi.market.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sayHi")
+public class HiWorld {
+    @GetMapping("/")
+    public String sayHi() {
+        return "Hi There!";
+    }
+}
