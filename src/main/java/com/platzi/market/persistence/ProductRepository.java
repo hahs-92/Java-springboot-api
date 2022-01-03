862a9939c30eb498e8f2e4a9687ac7ff229d380c
Alex <alexandre_2420@outlook.com>
2022-01-03 09:33:20 -0500

    implement @Repository: ProductRepository

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -24,5 +26,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getProductScarce(int stock) {
         return productCrudRepository.findByStockLessThanAndStatus(stock, true);
+    }
+
+    public Optional<Product> getProduct(int idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product saveProduct(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void deleteProduct(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
